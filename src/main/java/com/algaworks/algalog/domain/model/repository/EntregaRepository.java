fc65ae2c2bc91ea5ff903e6fa8eb3c3365315050
guiimagno <guilherme.mprata@gmail.com>
2021-10-23 15:35:45 -0300

    implementando solicitação de entrega

---
+++ src/main/java/com/algaworks/algalog/domain/model/repository/EntregaRepository.java
@@ -0,0 +1,7 @@
+package com.algaworks.algalog.domain.model.repository;
+
+import com.algaworks.algalog.domain.model.Entrega;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntregaRepository extends JpaRepository<Entrega, Long> {
+}
